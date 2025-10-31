dbd1453acfde95bc55596158a524af37f2143d36
OscarPena <oscarpena3799@gmail.com>
2025-10-31 12:21:34 -0600

    first file for commiting

---
+++ test/pageobjects/secure.page.js
@@ -0,0 +1,13 @@
+const { $ } = require('@wdio/globals')
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class SecurePage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+}
+
+module.exports = new SecurePage();
---
+++ test/specs/test.e2e.js
@@ -0,0 +1,15 @@
+const { expect } = require('@wdio/globals')
+const LoginPage = require('../pageobjects/login.page')
+const SecurePage = require('../pageobjects/secure.page')
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('standard_user', 'secret_sauce')
+     const inventory = await $('#inventory_container');
+     await expect(inventory).toBeExisting();
+    })
+})
+
+
